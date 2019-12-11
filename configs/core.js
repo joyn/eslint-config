2632ccec1892507f41d69db49e394e00511484e5
Mathias Schreck <mathias.schreck@joyn.de>
2019-12-11 14:26:54 +0100

    🐛 Make space-before-function-paren settings compatible with prettier

--- configs/core.js
+++ configs/core.js
@@ -333,7 +333,7 @@ module.exports = {
         'space-before-function-paren': [
             'error',
             {
-                anonymous: 'always',
+                anonymous: 'never',
                 named: 'never',
                 asyncArrow: 'always'
             }
