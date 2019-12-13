fd5c93fc172847557eb3b70a3be738e3c40700c6
Mathias Schreck <mathias.schreck@joyn.de>
2019-12-13 11:44:10 +0100

    🐛 Disable node/no-unsupported-features/es-syntax for esm environment

--- presets/esm.js
+++ presets/esm.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-    extends: ['../configs/es-modules.js']
+    extends: ['../configs/es-modules.js'],
+
+    rules: {
+        'node/no-unsupported-features/es-syntax': 'off'
+    }
 };
