355e911bace92bc3400d34981748213a9cf6c368
Diogo Azevedo <diogoazevedos@gmail.com>
2022-12-28 15:52:54 +0100

    Add removed core rules validation
    Co-authored-by: Hugo Goncalves <hugo79@hotmail.com>

--- test/coreSpec.js
+++ test/coreSpec.js
@@ -6,25 +6,25 @@ const eslintReplacements = require('../node_modules/eslint/conf/replacements.jso
 const coreConfig = require('../configs/core');
 const { testCoreRulesConfigured } = require('./macros');
 
-test('all eslint core rules are configured', testCoreRulesConfigured, {
+test('all core rules are configured', testCoreRulesConfigured, {
     rules: eslintDefaults.rules,
     ruleConfigSet: coreConfig.rules
 });
 
-test('does not contain configs for core rules that do not exist (anymore)', (t) => {
-    const rules = Object.keys(coreConfig.rules);
+test('does not contain removed core rules', (t) => {
+    const rules = Object.keys(eslintDefaults.rules);
     const configuredRules = Object.keys(coreConfig.rules);
 
     configuredRules.forEach((ruleName) => {
         const isCoreRule = !ruleName.includes('/');
 
         if (isCoreRule) {
-            t.true(rules.includes(ruleName));
+            t.true(rules.includes(ruleName), `Rule ${ruleName} can be removed`);
         }
     });
 });
 
-test('does not contain configs for deprecated core rules', (t) => {
+test('does not contain replaced core rules', (t) => {
     const rules = Object.keys(eslintReplacements.rules);
     const configuredRules = Object.keys(coreConfig.rules);
 
